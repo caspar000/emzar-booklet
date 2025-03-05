615e0d1a99b4af5d885bf163f7ae523b3326350b
caspar000 <nikolaev2000@protonmail.com>
2025-03-05 23:35:07 +0400

    Quartz sync: Mar 5, 2025, 11:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "caspar000.github.io/emzar-booklet/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
